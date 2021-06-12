46067ab842ec43d3f45e0a3b6dfc24495af794b8
Safiyya H <safiyya.hasanova@gmail.com>
2021-06-12 12:29:40 -0500

    current hour and changes to color

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,21 @@ var idArr = ['#8', '#9', '#10', '#11', '#12', '#13', '#14', '#15', '#16', '#17']
 
     // declare current hour and format to same form as eventHour
 var currentHour = parseInt(moment().format('H'));
+// iterate thru all event blocks
+for (let i = 0; i < idArr.length; i++) {
+    var findTB = $(idArr[i]);
+
+    // declare a var that selects the hour from each time-block and format it
+    var eventHour = findTB.siblings('.hour').text().trim();
+    eventHour = parseInt(moment(eventHour, 'h:mm A').format('H'));
+
+    // setting conditions to change the colors of time-blocks
+    if (currentHour === eventHour) {
+        findTB.addClass('present').removeClass('past future');
+    } else if (currentHour > eventHour) {
+        findTB.addClass('past').removeClass('present future disabled');
+    } else {
+        findTB.addClass('future').removeClass('past present');
+    };
+}
+// END color changing based on time
